7c7e3fbb5c066d30c335cdfef0b15388f3917747
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-20 19:44:41 -0500

    master

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { auth } from './firebase/init';
+import { auth, db } from './firebase/init';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 
 function App() {
