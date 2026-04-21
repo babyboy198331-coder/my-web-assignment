da7d36077d26c6dfec68d02418f9d2cc754eebb5
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-20 20:02:28 -0500

    master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import { auth, db } from './firebase/init';
+import { collection, addDoc } from "firebase/firestore";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const [user, setUser] = React.useState({});
   const [loading, setLoading] = React.useState(true);
+
+  function createPost() {
+    const post = {
+      title: "Land a $80k job",
+      description: "Finish Frontend Simplified",
+    };
+    addDoc(collection(db, "posts"), post)
+  }
 
 React.useEffect(() => {
 onAuthStateChanged(auth, (user) => {
@@ -48,6 +57,7 @@ function logout() {
         <button onClick={login}>Login</button>
         <button onClick={logout}>Logout</button>
         {loading ? 'loading...' : user.email}
+        <button onClick={createPost}>Create Post</button>
     </div>
   );
 }
